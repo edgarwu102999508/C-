61d698aa5dc4e07f608af9cb1e0fbdb3a39950ad
edgarwu <wuzhengjie@gmail.com>
2020-03-03 14:40:22 +1100

    IntroProgSelection

---
+++ IntroProgSelection2020/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IntroProgSelection2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 16;
+            int legalAge = 16;
+            if (age >= legalAge) {
+                Console.WriteLine("You can leave school.");
+            }else {
+                System.Console.WriteLine("Must stay at school.");
+            }
+            System.Console.WriteLine("Terminated");            
+        }
+    }
+}
